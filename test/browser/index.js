2cf5197d14f302767c49947e6b0c9eeb4d29dd33
Ryan Roemer <ryan@loose-bits.com>
2018-04-13 13:17:12 -0700

    Build only a single test bundle for karma.

---
+++ test/browser/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Polyfills for IE9 in React 16.
+require('core-js/es6/map');
+require('core-js/es6/set');
+require('core-js/es6/weak-map');
+
+// Re-use node tests.
+const testsContext = require.context('../node', true, /\.spec\.js$/);
+testsContext.keys().forEach(testsContext);
--- test/browser/karma.conf.js
+++ test/browser/karma.conf.js
@@ -1,23 +1,16 @@
 'use strict';
 
 const path = require('path');
-const CORE_JS_ROOT = path.dirname(require.resolve('core-js/package.json'));
 
 module.exports = function(config) {
   config.set({
     basePath: '../..',
     frameworks: ['mocha'],
     files: [
-      // Polyfills for IE9 in React 16.
-      require.resolve('core-js/es6/map'),
-      require.resolve('core-js/es6/set'),
-      require.resolve('core-js/es6/weak-map'),
-      // Re-use node tests.
-      'test/node/**/*.spec.js'
+      'test/browser/index.js'
     ],
     preprocessors: {
-      [path.join(CORE_JS_ROOT, 'es6/**/*.js')]: ['webpack'],
-      'test/**/*.js': ['webpack']
+      'test/browser/index.js': ['webpack']
     },
     webpack: {
       mode: 'development',
@@ -31,7 +24,7 @@ module.exports = function(config) {
           {
             test: /\.js$/,
             enforce: 'pre',
-            include: path.resolve(__dirname, '../node/'),
+            include: path.resolve(__dirname),
             loader: 'babel-loader',
             options: {
               presets: ['babel-preset-env']
