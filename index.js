e624f671a7fe0d21809101ac8e47bd3160af65d7
samwhale <samuel.estrella@formidable.com>
2018-06-04 13:31:35 -0600

    Add updates from fast-deep-equal@2.0.1.
    * null and object comparison
    * new behavior: different functions that are not equal
    * new behavior: handle NaN

--- index.js
+++ index.js
@@ -7,43 +7,43 @@ var hasProp = Object.prototype.hasOwnProperty;
 function equal(a, b) {
   if (a === b) return true;
 
-  var arrA = isArray(a)
-    , arrB = isArray(b)
-    , i
-    , length
-    , key;
+  if (a && b && typeof a == 'object' && typeof b == 'object') {
+    var arrA = isArray(a)
+      , arrB = isArray(b)
+      , i
+      , length
+      , key;
 
-  if (arrA && arrB) {
-    length = a.length;
-    if (length != b.length) return false;
-    for (i = 0; i < length; i++)
-      if (!equal(a[i], b[i])) return false;
-    return true;
-  }
+    if (arrA && arrB) {
+      length = a.length;
+      if (length != b.length) return false;
+      for (i = length; i-- !== 0;)
+        if (!equal(a[i], b[i])) return false;
+      return true;
+    }
 
-  if (arrA != arrB) return false;
+    if (arrA != arrB) return false;
 
-  var dateA = a instanceof Date
-    , dateB = b instanceof Date;
-  if (dateA != dateB) return false;
-  if (dateA && dateB) return a.getTime() == b.getTime();
+    var dateA = a instanceof Date
+      , dateB = b instanceof Date;
+    if (dateA != dateB) return false;
+    if (dateA && dateB) return a.getTime() == b.getTime();
 
-  var regexpA = a instanceof RegExp
-    , regexpB = b instanceof RegExp;
-  if (regexpA != regexpB) return false;
-  if (regexpA && regexpB) return a.toString() == b.toString();
+    var regexpA = a instanceof RegExp
+      , regexpB = b instanceof RegExp;
+    if (regexpA != regexpB) return false;
+    if (regexpA && regexpB) return a.toString() == b.toString();
 
-  if (a instanceof Object && b instanceof Object) {
     var keys = keyList(a);
     length = keys.length;
 
     if (length !== keyList(b).length)
       return false;
 
-    for (i = 0; i < length; i++)
+    for (i = length; i-- !== 0;)
       if (!hasProp.call(b, keys[i])) return false;
 
-    for (i = 0; i < length; i++) {
+    for (i = length; i-- !== 0;) {
       key = keys[i];
       if (key === '_owner' && a.$$typeof && a._store) {
         // React-specific: avoid traversing React elements' _owner.
@@ -60,7 +60,7 @@ function equal(a, b) {
     return true;
   }
 
-  return false;
+  return a!==a && b!==b;
 }
 
 module.exports = function exportedEqual(a, b) {
