aeb14c135af9a6c1402bfd40553d245807ad6050
Ryan Roemer <ryan.roemer@formidable.com>
2019-12-18 10:55:01 -0800

    More dep upgrades.

--- test/browser/index.js
+++ test/browser/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
 // Polyfills for IE9 in React 16.
-require('core-js/es6/map');
-require('core-js/es6/set');
-require('core-js/es6/weak-map');
+require('core-js/features/map');
+require('core-js/features/set');
+require('core-js/features/weak-map');
 
 // Re-use node tests.
 const testsContext = require.context('..', true, /\.spec\.js$/);
