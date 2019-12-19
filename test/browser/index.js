d62fa578495620ff639ce5b7acf0eda85f59e7ca
Ryan Roemer <ryan.roemer@formidable.com>
2019-12-19 15:00:33 -0800

    Add flags polyfill.

--- test/browser/index.js
+++ test/browser/index.js
@@ -5,6 +5,7 @@ require('core-js/features/map');
 require('core-js/features/set');
 require('core-js/features/weak-map');
 require('core-js/features/symbol');
+require('core-js/features/regexp/flags');
 
 // Re-use node tests.
 const testsContext = require.context('..', true, /\.spec\.js$/);
