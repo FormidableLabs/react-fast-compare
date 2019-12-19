23892940d132f416167d7253cfeb614ef35e5956
Ryan Roemer <ryan.roemer@formidable.com>
2019-12-19 14:02:33 -0800

    Add symbol polyfill for ie

--- test/browser/index.js
+++ test/browser/index.js
@@ -4,6 +4,7 @@
 require('core-js/features/map');
 require('core-js/features/set');
 require('core-js/features/weak-map');
+require('core-js/features/symbol');
 
 // Re-use node tests.
 const testsContext = require.context('..', true, /\.spec\.js$/);
