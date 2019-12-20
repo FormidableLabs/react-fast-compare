6a827d1252b1001ff63a47b165c0b26a00ec5faa
Stephen Richardson <srichardson@reactual.io>
2019-12-19 17:21:56 -0700

    Improve performance by avoiding extra work
    This version avoids work in a few ways:
    
    1. Not declaring extra variables
    2. Avoiding 2 `instanceof` checks if not needed.
    3. Using while loops without an extra comparison
    
    There may be an extra gain on line 12, if the data isn't expected to have a lot of `null` values,
    you could potentially do the object checks first, then the truthy `a && b` check.

--- index.js
+++ index.js
@@ -10,60 +10,51 @@ function equal(a, b) {
   if (a === b) return true;
 
   if (a && b && typeof a == 'object' && typeof b == 'object') {
-    var arrA = isArray(a)
-      , arrB = isArray(b)
-      , i
-      , length
-      , key;
+    var i;
 
-    if (arrA && arrB) {
-      length = a.length;
-      if (length != b.length) return false;
-      for (i = length; i-- !== 0;)
-        if (!equal(a[i], b[i])) return false;
+    if (isArray(a)) {
+      if (!isArray(b)) return false;
+      if (a.length !== b.length) return false;
+      i = a.length;
+      while (i--) if (!equal2(a[i], b[i])) return false;
       return true;
     }
 
-    if (arrA != arrB) return false;
+    if (a instanceof Date) {
+      if (!(b instanceof Date)) return false;
+      return a.getTime() == b.getTime();
+    }
 
-    var dateA = a instanceof Date
-      , dateB = b instanceof Date;
-    if (dateA != dateB) return false;
-    if (dateA && dateB) return a.getTime() == b.getTime();
-
-    var regexpA = a instanceof RegExp
-      , regexpB = b instanceof RegExp;
-    if (regexpA != regexpB) return false;
-    if (regexpA && regexpB) return a.toString() == b.toString();
+    if (a instanceof RegExp) {
+      if (!(b instanceof RegExp)) return false;
+      return a.toString() == b.toString();
+    }
 
     var keys = keyList(a);
-    length = keys.length;
 
-    if (length !== keyList(b).length)
-      return false;
+    if (keys.length !== keyList(b).length) return false;
 
-    for (i = length; i-- !== 0;)
-      if (!hasProp.call(b, keys[i])) return false;
+    i = keys.length;
+
+    while (i--) if (!hasProp.call(b, keys[i])) return false;
     // end fast-deep-equal
 
     // start react-fast-compare
     // custom handling for DOM elements
-    if (hasElementType && a instanceof Element)
-      return false;
+    if (hasElementType && a instanceof Element) return false;
 
     // custom handling for React
-    for (i = length; i-- !== 0;) {
-      key = keys[i];
-      if (key === '_owner' && a.$$typeof) {
-        // React-specific: avoid traversing React elements' _owner.
-        //  _owner contains circular references
-        // and is not needed when comparing the actual elements (and not their owners)
-        // .$$typeof and ._store on just reasonable markers of a react element
-        continue;
-      } else {
-        // all other properties should be traversed as usual
-        if (!equal(a[key], b[key])) return false;
-      }
+    i = keys.length;
+
+    while (i--) {
+      // React-specific: avoid traversing React elements' _owner.
+      //  _owner contains circular references
+      // and is not needed when comparing the actual elements (and not their owners)
+      // .$$typeof and ._store on just reasonable markers of a react element
+      if (keys[i] === '_owner' && a.$$typeof) continue;
+
+      // all other properties should be traversed as usual
+      if (!equal2(a[keys[i]], b[keys[i]])) return false;
     }
     // end react-fast-compare
 
