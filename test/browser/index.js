6c7b60e32afc988bd55a78d1f40a5ddfe70512f6
Ryan Roemer <ryan.roemer@formidable.com>
2020-05-08 14:20:24 -0700

    Fix win ci

--- test/browser/index.js
+++ test/browser/index.js
@@ -1,11 +1,14 @@
 'use strict';
 
-// Polyfills for IE in React 16.
+// Polyfills for IE in React 16 and other dependencies.
+require('core-js/features/array/from');
+require('core-js/features/object/entries');
+require('core-js/features/promise');
 require('core-js/features/map');
 require('core-js/features/set');
 require('core-js/features/weak-map');
-require('core-js/features/symbol');
 require('core-js/features/regexp/flags');
+require('core-js/features/symbol');
 
 // Re-use node tests.
 const testsContext = require.context('..', true, /\.spec\.js$/);
