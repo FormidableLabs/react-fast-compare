d87b34a54a7f56de4a641e6bbd612a5c16a9cd9a
Kylie Stewart <kylie@kyliestewart.tech>
2020-05-15 11:00:03 -0600

    tsconfig cleanup

--- test/typescript/index.js
+++ test/typescript/index.js
@@ -1,33 +1,49 @@
-import React from "react";
-import { useSelector } from "react-redux";
-const equal = require("../..");
-// const equal = (a: any, b: any) => { return true; }
-/**
- * - [ ] Component to wrap the to-dos (Container)
- *       Maps over an array of random strings to pass into the Child component
- * - [ ] Component to render each to-do individually (Child)
- */
-const testArr = [
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+exports.__esModule = true;
+var react_1 = require("react");
+var react_redux_1 = require("react-redux");
+var equal = require("../..");
+var testArr = [
     { text: "green", id: "23" },
     { text: "sunshine", id: "1" },
     { text: "mountain", id: "11" },
     { text: "air", id: "8" },
     { text: "plants", id: "9" }
 ];
-class TestChild extends React.Component {
-    // is this doing anything for us?
-    // should the container be improved to ensure that this update is happening?
-    // should the container be within a container? 🤔
-    shouldComponentUpdate(nextProps) {
+var TestChild = /** @class */ (function (_super) {
+    __extends(TestChild, _super);
+    function TestChild() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    TestChild.prototype.shouldComponentUpdate = function (nextProps) {
         return !equal(this.props, nextProps);
+    };
+    TestChild.prototype.render = function () {
+        var todo = react_redux_1.useSelector(function (state) { return state.todos[props.id]; });
+        return <div>{todo}</div>;
+    };
+    return TestChild;
+}(react_1["default"].Component));
+var TestContainer = /** @class */ (function (_super) {
+    __extends(TestContainer, _super);
+    function TestContainer() {
+        return _super !== null && _super.apply(this, arguments) || this;
     }
-    render() {
-        const todo = useSelector((state) => state.todos[props.id]);
-        return React.createElement("div", null, todo);
-    }
-}
-class TestContainer extends React.Component {
-    render() {
-        return (testArr.map(item => React.createElement(TestChild, { todo: item })));
-    }
-}
+    TestContainer.prototype.render = function () {
+        return (testArr.map(function (item) { return <TestChild todo={item}/>; }));
+    };
+    return TestContainer;
+}(react_1["default"].Component));
