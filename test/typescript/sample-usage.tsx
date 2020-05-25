36878b4ff85cbb32f3f54da7f0b5c91f78227b14
Kylie Stewart <kylie@kyliestewart.tech>
2020-05-25 14:15:25 -0600

    Add a test for react-redux's useSelector hook

--- test/typescript/sample-usage.tsx
+++ test/typescript/sample-usage.tsx
@@ -2,6 +2,7 @@
 // This is compiled using `tsc` in our `test-ts-usage` script
 import React from 'react';
 import equal from '../../index.js';
+import { useSelector } from 'react-redux';
 
 type ITodo = {
   text: string;
@@ -14,26 +15,44 @@ const testArr: ITodo[] = [
   { text: 'mountain', id: '11' },
   { text: 'air', id: '8' },
   { text: 'plants', id: '9' },
+  { text: 'air', id: '8' },
+  { text: 'air', id: '8' },
 ];
 
-type IProps = {
+type IChildProps = {
   todo: ITodo;
 };
 
-class TestChild extends React.Component<IProps> {
-  shouldComponentUpdate(nextProps: IProps) {
+class TestChild extends React.Component<IChildProps> {
+  shouldComponentUpdate(nextProps: IChildProps) {
     return !equal(this.props, nextProps);
   }
 
   render() {
     const todo = this.props.todo;
+
     return <div>{todo.text}</div>;
   }
 }
 
-class TestContainer extends React.Component {
+type IContainerState = { completed: ITodo[] };
+
+class TestContainer extends React.Component<{}, IContainerState> {
   render() {
-    return testArr.map((item) => <TestChild key={item.id} todo={item} />);
+    const numberOfOverlappingItems: number = useSelector(
+      (state) => this.state.completed.length
+    );
+
+    return (
+      <div>
+        Overlap: {numberOfOverlappingItems}
+        <div>
+          {testArr.map((item) => (
+            <TestChild key={item.id} todo={item} />
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 
