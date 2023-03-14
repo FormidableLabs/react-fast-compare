d9146f309bd214a80a1408d1ca5633cc42393a07
Ryan Roemer <ryan.roemer@formidable.com>
2023-03-14 13:27:15 -0700

    Add new tests and fix

--- test/node/tests.js
+++ test/node/tests.js
@@ -64,9 +64,49 @@ const react = [
   }
 ];
 
+// Additional customized behavior.
+const custom = [
+  {
+    description: 'Custom tests',
+    tests: [
+      {
+        description: 'Object.create(null) equal',
+        value1: Object.create(null),
+        value2: Object.create(null),
+        equal: true
+      },
+      {
+        description: 'Object.create(null) unequal to empty object',
+        value1: Object.create(null),
+        value2: {},
+        equal: false
+      },
+      {
+        description: 'Object.create(null) unequal to non-empty object',
+        value1: Object.create(null),
+        value2: { a: 1 },
+        equal: false
+      },
+      {
+        description: 'Object.create(null) equal to vanilla deep objects',
+        value1: Object.assign(Object.create(null), { a: 1, b: { c: true } }),
+        value2: { a: 1, b: { c: true } },
+        equal: false
+      },
+      {
+        description: 'Object.create(null) equal for deep objects',
+        value1: Object.assign(Object.create(null), { a: 1, b: { c: true } }),
+        value2: Object.assign(Object.create(null), { b: { c: true }, a: 1 }),
+        equal: true
+      }
+    ]
+  }
+];
+
 module.exports = {
   generic,
   es6,
   react,
-  all: [...generic, ...es6, ...react],
+  custom,
+  all: [].concat(generic, es6, react, custom),
 };
